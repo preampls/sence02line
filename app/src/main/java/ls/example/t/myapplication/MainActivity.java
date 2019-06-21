480b4e3cef988510374b955b7587ba3af9a3df98
nhsoft.ls <ziJd6ZzFgsGYsHpVRuQy>
2019-06-21 15:20:35 +0800

    first

---
+++ app/src/main/java/ls/example/t/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package ls.example.t.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/ls/example/t/myapplication/chapater1/Chapater1_Activity.java
@@ -0,0 +1,16 @@
+package ls.example.t.myapplication.chapater1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ls.example.t.myapplication.R;
+
+public class Chapater1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapater1_);
+
+    }
+}
