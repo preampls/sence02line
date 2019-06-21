720ab132a7608186d9ae0d978f01d6f3c30933ef
nhsoft.ls <ziJd6ZzFgsGYsHpVRuQy>
2019-06-21 15:46:39 +0800

    test 03

--- app/src/main/java/ls/example/t/myapplication/chapater1/Chapater1_Activity.java
+++ app/src/main/java/ls/example/t/myapplication/chapater1/Chapater1_Activity.java
@@ -11,5 +11,8 @@ public class Chapater1_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapater1_);
+
+
+
     }
 }
