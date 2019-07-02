a2b09baaa9e141c379c9ff970006ff88e1ab4d70
nhsoft.ls <ziJd6ZzFgsGYsHpVRuQy>
2019-07-02 09:35:50 +0800

    2019-07-01

--- app/src/main/java/ls/example/t/zero2line/chapater2/Chapater2Activity.java
+++ app/src/main/java/ls/example/t/zero2line/chapater2/Chapater2Activity.java
@@ -2,18 +2,28 @@ package ls.example.t.zero2line.chapater2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import ls.example.t.zero2line.base.BaseActivity;
 import ls.t.zero2line.R;
 
 
-public class Chapater2Activity extends AppCompatActivity {
+public class Chapater2Activity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapater2);
 
-
+        findViewById(R.id.radio_button_test).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.radio_button_test:
+                jumpActivity(Chapater2_1Activity.class);
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/ls/example/t/zero2line/chapater2/Chapater2_1Activity.java
@@ -0,0 +1,21 @@
+package ls.example.t.zero2line.chapater2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ls.t.zero2line.R;
+
+public class Chapater2_1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_chapater2);
+
+    }
+
+}
